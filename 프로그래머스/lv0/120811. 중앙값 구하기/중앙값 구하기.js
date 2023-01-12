79a288701a0e60e57cfd7bdaa5a8084d63d1cb33
KimMinchan <83770790+KimMinchan95@users.noreply.github.com>
2023-01-13 02:35:08 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,4 +1,3 @@
 function solution(array) {
-    array.sort((a, b) => a - b);
-    return array[Math.floor(array.length / 2)];
+    return array.sort((a, b) => a - b)[Math.floor(array.length / 2)];
 }
