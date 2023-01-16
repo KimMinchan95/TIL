6a347096377d4bd8b49080a622187d0ec08ed754
KimMinchan <83770790+KimMinchan95@users.noreply.github.com>
2023-01-17 00:43:02 +0900

    [level 0] Title: 순서쌍의 개수, Time: 4.75 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -2,7 +2,7 @@ function solution(n) {
     let result = 0;
     
     for (let i = 1; i <= n; i++) {
-        if (parseInt(n / i) === n / i) result++;    
+        if (!(n % i)) result++;    
     }
     
     return result;
