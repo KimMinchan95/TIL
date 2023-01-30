03d2a902698374e444f687177326327100835e71
KimMinchan <83770790+KimMinchan95@users.noreply.github.com>
2023-01-30 22:11:44 +0900

    [level 2] Title: 스킬트리, Time: 0.28 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.js
@@ -0,0 +1,10 @@
+function solution(skill, skill_trees) {
+    const regexp = new RegExp (`[${skill}]`);
+    return skill_trees.reduce((acc, cur) => {
+        const filteredCur = [...cur].filter(s => s.match(regexp)).join('');
+        
+        if(skill.indexOf(filteredCur) === 0) acc++;
+        
+        return acc;
+    }, 0);
+}
