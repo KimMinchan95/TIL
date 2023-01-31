b64e7d72f79e29c3546806008c69ff02a15a646b
KimMinchan <83770790+KimMinchan95@users.noreply.github.com>
2023-02-01 00:17:48 +0900

    [level 0] Title: A로 B 만들기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
@@ -1,12 +1,6 @@
 function solution(before, after) {
-    let result = 1;
+    const sortedBefore = [...before].sort().join('');
+    const sotredAfter = [...after].sort().join('');
     
-    const sortedBefore = [...before].sort();
-    const sotredAfter = [...after].sort();
-    
-    sortedBefore.forEach((string, i) => {
-        if (string !== sotredAfter[i]) result = 0;
-    });
-    
-    return result;
+    return sortedBefore === sotredAfter ? 1 : 0;
 }
