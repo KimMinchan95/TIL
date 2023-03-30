22f713244d4869b8cf3f4dce43abe73f39fc8f29
KimMinchan <83770790+KimMinchan95@users.noreply.github.com>
2023-03-30 20:44:47 +0900

    [unrated] Title: 기사단원의 무기, Time: 1.38 ms, Memory: 35.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
@@ -0,0 +1,29 @@
+const caculateKnightPower = num => {
+    let result = 0;
+    
+    for (let i = 1; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            if (num / i === i) {
+                result += 1;
+            } else {
+                result += 2;  
+            }
+        }
+    }
+    
+    return result;
+}
+
+function solution(number, limit, power) {
+    const numbers = [];
+    
+    for (let i = 1; i <= number; i++) {
+        numbers.push(i);
+    }
+    
+    return numbers.reduce((acc, cur) => {
+        const knightPower = caculateKnightPower(cur);
+        
+        return acc += knightPower > limit ?  power : knightPower;
+    }, 0);
+}
