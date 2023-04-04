6b06e60abf3dc5d5923bfa2fafc2148727bc9991
KimMinchan <83770790+KimMinchan95@users.noreply.github.com>
2023-04-04 22:30:04 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -0,0 +1,28 @@
+function solution(new_id) {
+    // 1 단계
+    let id = new_id.toLowerCase();
+    
+    // 2 단계
+    id = id.replace(/[^\w\-\_\.]/g, '');
+    
+    // 3 단계
+    id = id.replace(/\.{1,}/g, '.');
+    
+    // 4 단계
+    id = id.replace(/^\./, '');
+    id = id.replace(/\.$/, '');
+    
+    // 5 단계
+    if (!id) id = 'a';
+
+    // 6 단계
+    id = id.slice(0, 15);
+    id = id.replace(/\.$/, '');
+    
+    // 7 단계
+    if (id.length < 3) {
+        id = id.padEnd(3, id[id.length - 1]);
+    }
+    
+    return id;
+}
