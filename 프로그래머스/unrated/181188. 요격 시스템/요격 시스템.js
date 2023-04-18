3181493b0f2f802b5c834bf304fc3546d3eee701
KimMinchan <83770790+KimMinchan95@users.noreply.github.com>
2023-04-18 22:51:31 +0900

    [unrated] Title: 요격 시스템, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.js
@@ -0,0 +1,17 @@
+function solution(targets) {
+    let result = 0;
+    
+    targets.sort((a, b) => a[1] === b[1] ?  a[0] - b[0] : a[1] - b[1]);
+    
+    let memo = -1;
+    
+    targets.forEach(([start, end]) => {
+        if (start < memo) {
+            return;
+        }
+        memo = end;
+        result++;
+    });
+    
+    return result;
+}
