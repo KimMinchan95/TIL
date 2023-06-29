475b6f8a4846d856187f0ae789d4dcdd26e18c36
KimMinchan <83770790+KimMinchan95@users.noreply.github.com>
2023-06-29 22:08:01 +0900

    [unrated] Title: 바탕화면 정리, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.js
+++ 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.js
@@ -1,15 +1,14 @@
 function solution(wallpaper) {
     return wallpaper.reduce((acc, cur, i) => {
-        const first = [i, cur.indexOf('#')];
-        const last = [i + 1, cur.lastIndexOf('#') + 1];
+        const vertax = [i, cur.indexOf('#'), i + 1, cur.lastIndexOf('#') + 1];
         
         if (cur.indexOf('#') === -1) return acc;
         
-        if (first[0] < acc[0]) acc[0] = first[0];
-        if (first[1] < acc[1]) acc[1] = first[1];
+        if (vertax[0] < acc[0]) acc[0] = vertax[0];
+        if (vertax[1] < acc[1]) acc[1] = vertax[1];
         
-        if (last[0] > acc[2]) acc[2] = last[0];
-        if (last[1] > acc[3]) acc[3] = last[1];
+        if (vertax[2] > acc[2]) acc[2] = vertax[2];
+        if (vertax[3] > acc[3]) acc[3] = vertax[3];
         
         return acc;
     }, [wallpaper.length + 1, wallpaper[0].length + 1, -1, -1]);
