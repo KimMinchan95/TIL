b8fb0cf93ac64602d07f7491e8a5fd452ab9233c
KimMinchan <83770790+KimMinchan95@users.noreply.github.com>
2023-07-02 18:18:03 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 52.86 ms, Memory: 41.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.js
@@ -0,0 +1,24 @@
+function solution(rows, columns, queries) {
+    const cube = Array.from(Array(rows), (_, idx) => Array(columns).fill().map((_, i) => idx * columns + i + 1));
+    
+    return queries.reduce((acc, cur) => {
+        const [y1, x1, y2, x2] = cur.map(el => el - 1);
+        
+        const arr = [];
+        
+        for (let i = x1; i < x2; i++) arr.push(cube[y1][i]);
+        for (let i = y1; i < y2; i++) arr.push(cube[i][x2]);
+        for (let i = x2; i > x1; i--) arr.push(cube[y2][i]);
+        for (let i = y2; i > y1; i--) arr.push(cube[i][x1]);
+        
+        acc.push(Math.min(...arr));
+        arr.unshift(arr.pop());
+        
+        for (let i = x1; i < x2; i++) cube[y1][i] = arr.shift();
+        for (let i = y1; i < y2; i++) cube[i][x2] = arr.shift();
+        for (let i = x2; i > x1; i--) cube[y2][i] = arr.shift();
+        for (let i = y2; i > y1; i--) cube[i][x1] = arr.shift();
+            
+        return acc;
+    }, []);
+}
