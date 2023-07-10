7fb4980b3dbd81832437a1574bd1a7046920276c
KimMinchan <83770790+KimMinchan95@users.noreply.github.com>
2023-07-11 00:40:29 +0900

    [unrated] Title: 대충 만든 자판, Time: 0.75 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.js
@@ -0,0 +1,25 @@
+const maxNum = Number.MAX_SAFE_INTEGER;
+
+function solution(keymap, targets) {
+    const keyMap = keymap.reduce((acc, key) => {
+        [...key].forEach((cur, idx) => {
+            const getIdx = acc.get(cur) ?? maxNum;
+            acc.set(cur, getIdx > idx + 1 ? idx + 1 : getIdx);
+        });
+        
+        return acc;
+    }, new Map());
+    
+    return targets.map(target => {
+        let result = 0;
+        for (let i = 0; i < target.length; i++) {
+            const cur = target[i];
+            result += keyMap.get(cur);
+            if (!keyMap.get(cur)) {
+                result = null;
+                break;
+            };
+        }
+        return result ?? -1;
+    });
+}
