dae5c737d02c2ec16f735d88cee25b4cf7514ab7
KimMinchan <83770790+KimMinchan95@users.noreply.github.com>
2023-07-31 23:45:00 +0900

    [unrated] Title: 둘만의 암호, Time: 0.27 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.js
@@ -0,0 +1,20 @@
+const ALPHABET_LIST = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+function solution(s, skip, index) {
+    let result = '';
+    
+    for (let i = 0; i < s.length; i++) {
+        const str = s[i];
+        let idx = ALPHABET_LIST.findIndex(cur => cur === str);
+        let last = index;
+        while (last) {
+            idx++;
+            if (idx === 26) idx = 0;
+            if(skip.includes(ALPHABET_LIST[idx])) continue;
+            last--;
+        }
+        result += ALPHABET_LIST[idx];
+    }
+    
+    return result;
+}
