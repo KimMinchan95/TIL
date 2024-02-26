9e83396c948e14e18dd412f74ae1d521effd44ee
KimMinchan <83770790+KimMinchan95@users.noreply.github.com>
2024-02-26 21:54:29 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    let result = 0;
+    
+    if (n % 2) {
+        for (let i = 1; i <= n; i += 2) {
+            result += i;
+        }
+    } else {
+        for (let i = 2; i <= n; i += 2) {
+            result += i**2;
+        }
+    }
+    
+    return result;
+}
