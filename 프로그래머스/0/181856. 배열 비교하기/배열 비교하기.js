727765cf98ef0a6905c34190dc6f7a69401d69db
KimMinchan <83770790+KimMinchan95@users.noreply.github.com>
2024-03-18 07:09:19 +0900

    [level 0] Title: 배열 비교하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
@@ -1,12 +1,14 @@
+const sumArr = (arr) => arr.reduce((acc, cur) => acc + cur, 0);
+
+
 function solution(arr1, arr2) {
-    // 배열의 길이 계산
-    const arr1Len = arr1.length
-    const arr2Len = arr2.length
-    if(arr1Len !== arr2Len) return arr1Len > arr2Len ? 1 : -1
+    const arr1Len = arr1.length;
+    const arr2Len = arr2.length;
+    if (arr1Len !== arr2Len) return arr1Len > arr2Len ? 1 : -1;
     
-    // 요소의 합 계산
-    const arr1Sum = arr1.reduce((acc, cur) => acc+cur, 0) 
-    const arr2Sum = arr2.reduce((acc, cur) => acc+cur, 0)
-    if(arr1Sum === arr2Sum) return 0
-    return arr1Sum > arr2Sum ? 1: -1
+    const sum1 = sumArr(arr1);
+    const sum2 = sumArr(arr2);
+
+    if (sum1 === sum2) return 0;
+    return sum1 > sum2 ? 1 : -1;
 }
