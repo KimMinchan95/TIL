372afa1702824f100b0e5c30b11ae07f8b24fc1a
KimMinchan <83770790+KimMinchan95@users.noreply.github.com>
2025-04-15 18:30:53 +0900

    [Bronze V] Title: 문자와 문자열, Time: 92 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.js
@@ -0,0 +1,18 @@
+const test = './question.txt';
+const submit = '/dev/stdin';
+const filePath = process.platform === 'linux' ? submit : test;
+
+// ** 파일 읽는 방식들 ** //
+// 한개
+const input = require('fs').readFileSync(filePath).toString().trim();
+// Row
+const inputRow = require('fs').readFileSync(filePath).toString().split(' ');
+// Column
+const inputCol = require('fs')
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split('\n');
+
+const [str, num] = inputCol;
+console.log(str.at(num - 1));
