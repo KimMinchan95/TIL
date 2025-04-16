c5b994b17531ff5f8ca9e1dd856609310ec58368
KimMinchan <83770790+KimMinchan95@users.noreply.github.com>
2025-04-16 19:58:42 +0900

    [Bronze II] Title: 상수, Time: 92 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.js
@@ -0,0 +1,23 @@
+const localPath = './question.txt';
+const testPath = '/dev/stdin';
+const isTest = process.platform === 'linux';
+const filePath = isTest ? testPath : localPath;
+
+// ** 파일 읽는 방식들 ** //
+// 한개
+const input = require('fs').readFileSync(filePath).toString().trim();
+// Row
+const inputRow = require('fs').readFileSync(filePath).toString().split(' ');
+// Column
+const inputCol = require('fs')
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split('\n');
+
+const [a, b] = input
+  .split(' ')
+  .map((cur) => cur.split('').reverse().join(''))
+  .map(Number);
+
+console.log(Math.max(a, b));
