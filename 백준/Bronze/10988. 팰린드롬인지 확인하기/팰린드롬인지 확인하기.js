6a8f99fa78007b519a92a9615863150126fffc91
KimMinchan <83770790+KimMinchan95@users.noreply.github.com>
2025-04-17 18:53:39 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 92 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.js
@@ -0,0 +1,18 @@
+const localPath = './question.txt';
+const testPath = '/dev/stdin';
+const isTest = process.platform === 'linux';
+const filePath = isTest ? testPath : localPath;
+
+// ** 파일 읽는 방식들 ** //
+// 한개
+const input = require('fs').readFileSync(filePath).toString().trim();
+// Row
+const inputRow = require('fs').readFileSync(filePath).toString().split(' ');
+// Column
+const inputCol = require('fs')
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split('\n');
+
+console.log(Number(input.split('').reverse().join('') === input));
