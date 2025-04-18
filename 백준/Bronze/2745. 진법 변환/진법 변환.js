dd9b21d457ce98bc491b4bb4620ce60a7fb7db64
KimMinchan <83770790+KimMinchan95@users.noreply.github.com>
2025-04-18 17:42:21 +0900

    [Bronze II] Title: 진법 변환, Time: 148 ms, Memory: 9644 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.js
@@ -0,0 +1,21 @@
+const localPath = './question.txt';
+const testPath = '/dev/stdin';
+const isTest = process.platform === 'linux';
+const filePath = isTest ? testPath : localPath;
+
+// ** 파일 읽는 방식들 ** //
+// 한개
+const input = require('fs').readFileSync(filePath).toString().trim();
+// Row
+const inputRow = require('fs').readFileSync(filePath).toString().split(' ');
+// Column
+const inputCol = require('fs')
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split('\n');
+// ** 파일 읽는 방식 끝 **//
+
+const [str, meth] = input.split(' ');
+const num = Number(meth);
+console.log(parseInt(str, num));
