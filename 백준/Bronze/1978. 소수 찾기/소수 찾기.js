412385f7b888eadb730c3464dc775249b59237bb
KimMinchan <83770790+KimMinchan95@users.noreply.github.com>
2025-04-19 10:14:10 +0900

    [Bronze II] Title: 소수 찾기, Time: 92 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.js
@@ -0,0 +1,50 @@
+// ** 파일 읽는 방식들 ** //
+const fileRead = (type) => {
+  // 로컬인지에 대한 설정
+  const localPath = './question.txt';
+  const testPath = '/dev/stdin';
+  const isTest = process.platform === 'linux';
+  const filePath = isTest ? testPath : localPath;
+
+  const fileRead = require('fs').readFileSync(filePath);
+  // 한개
+  if (type === TYPES.ONE) return fileRead.toString().trim();
+  // 한줄
+  if (type === TYPES.ROW) return fileRead.toString().split(' ');
+  // Column
+  if (type === TYPES.COL) return fileRead.toString().trim().split('\n');
+
+  throw new Error('파일 읽는 방식 다시 확인해라');
+};
+
+const TYPES = {
+  ONE: 'ONE',
+  ROW: 'ROW',
+  COL: 'COL',
+};
+
+//! 풀이
+const answer = () => {
+  //! 파일 읽기
+  const input = fileRead(TYPES.COL);
+
+  const [count, list] = input;
+
+  return list
+    .split(' ')
+    .map(Number)
+    .reduce((acc, cur) => {
+      if (cur === 1) return acc;
+
+      let decimal = true;
+      for (let i = 2; i <= Math.sqrt(cur); i++) {
+        if (cur % i) continue;
+        decimal = false;
+        break;
+      }
+      return acc + Number(decimal);
+    }, 0);
+};
+
+//! 정답 출력
+console.log(answer());
