87c8c06cedcf4f5b93ccdce704b5f343de02564d
KimMinchan <83770790+KimMinchan95@users.noreply.github.com>
2025-04-20 10:18:53 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 92 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.js
@@ -0,0 +1,35 @@
+// ** 파일 읽는 방식들 ** //
+const fileRead = (type) => {
+  // 로컬인지에 대한 설정
+  const localPath = './question.txt';
+  const testPath = '/dev/stdin';
+  const isTest = process.platform === 'linux';
+  const filePath = isTest ? testPath : localPath;
+
+  const fileRead = require('fs').readFileSync(filePath);
+  // 한개
+  if (type === TYPES.ONE) return fileRead.toString().trim();
+  // 한줄
+  if (type === TYPES.ROW) return fileRead.toString().split(' ');
+  // Column
+  if (type === TYPES.COL) return fileRead.toString().trim().split('\n');
+
+  throw new Error('파일 읽는 방식 다시 확인해라');
+};
+
+const TYPES = {
+  ONE: 'ONE',
+  ROW: 'ROW',
+  COL: 'COL',
+};
+
+//! 풀이
+const answer = () => {
+  //! 파일 읽기
+  const input = fileRead(TYPES.ONE);
+
+  return `${(input * (input - 1)) / 2}\n2`;
+};
+
+//! 정답 출력
+console.log(answer());
