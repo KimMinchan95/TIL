745cff6f0085f3360f28188e3df8279d3588fc22
KimMinchan <83770790+KimMinchan95@users.noreply.github.com>
2025-04-30 00:07:22 +0900

    [Silver IV] Title: 제로, Time: 172 ms, Memory: 16308 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.js
@@ -0,0 +1,48 @@
+const TYPES = {
+  SINGLE: 'SINGLE',
+  ROW: 'ROW',
+  COL: 'COL',
+};
+
+// ** 파일 읽는 방식들 ** //
+const fileRead = (type) => {
+  // 로컬인지에 대한 설정
+  const localPath = './question.txt';
+  const testPath = '/dev/stdin';
+  const isTest = process.platform === 'linux';
+  const filePath = isTest ? testPath : localPath;
+
+  const fileRead = require('fs').readFileSync(filePath);
+  // 한개
+  if (type === TYPES.SINGLE) return fileRead.toString().trim();
+  // 한줄
+  if (type === TYPES.ROW) return fileRead.toString().split(' ');
+  // Column
+  if (type === TYPES.COL) return fileRead.toString().trim().split('\n');
+
+  throw new Error('파일 읽는 방식 다시 확인해라');
+};
+
+//! 풀이
+const answer = () => {
+  //! 파일 읽기
+  const input = fileRead(TYPES.COL);
+
+  input.shift();
+
+  return input
+    .map(Number)
+    .reduce((acc, cur) => {
+      if (!cur) {
+        acc.pop();
+      } else {
+        acc.push(cur);
+      }
+
+      return acc;
+    }, [])
+    .reduce((acc, cur) => acc + cur, 0);
+};
+
+//! 정답 출력
+console.log(answer());
