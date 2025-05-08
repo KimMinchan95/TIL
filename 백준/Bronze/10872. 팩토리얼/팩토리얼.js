84981219ee119157c636f9f28f2c442862960a33
KimMinchan <83770790+KimMinchan95@users.noreply.github.com>
2025-05-08 19:46:11 +0900

    [Bronze III] Title: 팩토리얼, Time: 92 ms, Memory: 9596 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.js
@@ -0,0 +1,43 @@
+const TYPES = {
+  SINGLE: 'SINGLE',
+  ROW: 'ROW',
+  COL: 'COL',
+};
+
+// ** 파일 읽는 방식들 ** //
+const fileRead = (type) => {
+  // 로컬인지에 대한 설정
+  const localPath = './question.txt';
+  const testPath = '/dev/stdin';
+  const isTest = process.platform === 'linux';
+  const filePath = isTest ? testPath : localPath;
+
+  const fileRead = require('fs').readFileSync(filePath);
+  // 한개
+  if (type === TYPES.SINGLE) return fileRead.toString().trim();
+  // 한줄
+  if (type === TYPES.ROW) return fileRead.toString().split(' ');
+  // Column
+  if (type === TYPES.COL) return fileRead.toString().trim().split('\n');
+
+  throw new Error('파일 읽는 방식 다시 확인해라');
+};
+
+//! 풀이
+const answer = () => {
+  //! 파일 읽기
+  const input = fileRead(TYPES.SINGLE);
+
+  const num = Number(input);
+
+  let result = 1;
+
+  for (let i = 2; i <= num; i++) {
+    result *= i;
+  }
+
+  return result;
+};
+
+//! 정답 출력
+console.log(answer());
