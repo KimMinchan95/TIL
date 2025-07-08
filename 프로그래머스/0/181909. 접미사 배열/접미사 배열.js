291b8d8404433e3ed2879d2ad9b5f71015fbc74f
KimMinchan <83770790+KimMinchan95@users.noreply.github.com>
2025-07-09 00:36:11 +0900

    [level 0] Title: 접미사 배열, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
@@ -0,0 +1,9 @@
+function solution(my_string) {
+    var answer = [];
+    
+    for (let i = 0; i < my_string.length; i++) {
+        answer.push(my_string.slice(i));    
+    }
+    
+    return answer.sort();
+}
