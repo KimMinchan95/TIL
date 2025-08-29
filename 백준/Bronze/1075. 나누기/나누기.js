0115fa4d7904d199cf05945a002e16a63887a14c
KimMinchan <83770790+KimMinchan95@users.noreply.github.com>
2025-08-29 17:52:08 +0900

    [Bronze II] Title: 나누기, Time: 96 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/1075. 나누기/나누기.js
@@ -0,0 +1,42 @@
+const TYPES = {
+  SINGLE: "SINGLE",
+  ROW: "ROW",
+  COL: "COL",
+};
+
+// ** 파일 읽는 방식들 ** //
+const fileRead = (type) => {
+  // 로컬인지에 대한 설정
+  const localPath = "./question.txt";
+  const testPath = "/dev/stdin";
+  const isTest = process.platform === "linux";
+  const filePath = isTest ? testPath : localPath;
+
+  const fileRead = require("fs").readFileSync(filePath);
+  // 한개
+  if (type === TYPES.SINGLE) return fileRead.toString().trim();
+  // 한줄
+  if (type === TYPES.ROW) return fileRead.toString().split(" ");
+  // Column
+  if (type === TYPES.COL) return fileRead.toString().trim().split("\n");
+
+  throw new Error("파일 읽는 방식 다시 확인해라");
+};
+
+//! 풀이
+const answer = () => {
+  //! 파일 읽기
+  const input = fileRead(TYPES.COL);
+
+  const [a, b] = input.map(Number);
+  let result = parseInt(a / 100) * 100;
+
+  while (result % b !== 0) {
+    result++;
+  }
+
+  return (result % 100).toString().padStart(2, "0");
+};
+
+//! 정답 출력
+console.log(answer());
