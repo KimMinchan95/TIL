fbdf985b14ad97b217e375ef32ab77828992cf50
KimMinchan <83770790+KimMinchan95@users.noreply.github.com>
2025-08-30 18:10:44 +0900

    [Bronze II] Title: 하얀 칸, Time: 96 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.js
@@ -0,0 +1,46 @@
+const TYPES = {
+  SINGLE: "SINGLE",
+  ROW: "ROW",
+  COL: "COL",
+};
+
+// ** 파일 읽는 방식들 ** //
+const fileRead = (type) => {
+  // 로컬인지에 대한 설정
+  const localPath = "./question.txt";
+  const testPath = "/dev/stdin";
+  const isTest = process.platform === "linux";
+  const filePath = isTest ? testPath : localPath;
+
+  const fileRead = require("fs").readFileSync(filePath);
+  // 한개
+  if (type === TYPES.SINGLE) return fileRead.toString().trim();
+  // 한줄
+  if (type === TYPES.ROW) return fileRead.toString().split(" ");
+  // Column
+  if (type === TYPES.COL) return fileRead.toString().trim().split("\n");
+
+  throw new Error("파일 읽는 방식 다시 확인해라");
+};
+
+//! 풀이
+const answer = () => {
+  //! 파일 읽기
+  const input = fileRead(TYPES.COL);
+
+  return input.reduce((acc, cur, i) => {
+    return (
+      acc +
+      cur.split("").reduce((acc, cur, idx) => {
+        if ((i + idx + 1) % 2) {
+          if (cur === "F") acc++;
+        }
+
+        return acc;
+      }, 0)
+    );
+  }, 0);
+};
+
+//! 정답 출력
+console.log(answer());
