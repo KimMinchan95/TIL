840f306cbf89536908c7c892c8bd69d81a09fb7e
KimMinchan <83770790+KimMinchan95@users.noreply.github.com>
2025-09-14 11:33:44 +0900

    [Silver V] Title: 막대기, Time: 96 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.js
@@ -0,0 +1,54 @@
+const TYPES = {
+  SINGLE: "SINGLE",
+  ROW: "ROW",
+  COL: "COL",
+};
+
+// ** 파일 읽는 방식들 ** //
+const fileRead = (type) => {
+  // 로컬인지에 대한 설정
+  const localPath = "./question.txt";
+  const testPath = "/dev/stdin";
+  const isTest = process.platform === "linux";
+  const filePath = isTest ? testPath : localPath;
+
+  const fileRead = require("fs").readFileSync(filePath);
+  // 한개
+  if (type === TYPES.SINGLE) return fileRead.toString().trim();
+  // 한줄
+  if (type === TYPES.ROW) return fileRead.toString().split(" ");
+  // Column
+  if (type === TYPES.COL) return fileRead.toString().trim().split("\n");
+
+  throw new Error("파일 읽는 방식 다시 확인해라");
+};
+
+//! 풀이
+const answer = () => {
+  //! 파일 읽기
+  const input = fileRead(TYPES.SINGLE);
+
+  let current = Number(input);
+
+  let stick = 64;
+
+  const list = [];
+
+  while (stick >= 1) {
+    list.push(stick);
+    stick /= 2;
+  }
+
+  let count = 0;
+
+  list.forEach((cur) => {
+    if (current >= cur) {
+      current -= cur;
+      count++;
+    }
+  });
+  return count;
+};
+
+//! 정답 출력
+console.log(answer());
