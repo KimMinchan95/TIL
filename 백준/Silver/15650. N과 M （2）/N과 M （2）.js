8cf67a3ccc73820365369835d9ca47b050b55675
KimMinchan <83770790+KimMinchan95@users.noreply.github.com>
2025-10-28 19:53:22 +0900

    [Silver III] Title: N과 M (2), Time: 284 ms, Memory: 11884 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.js
@@ -0,0 +1,62 @@
+const TYPES = {
+  SINGLE: 'SINGLE',
+  ROW: 'ROW',
+  COL: 'COL',
+};
+
+// ** 파일 읽는 방식들 ** //
+const fileRead = (type) => {
+  // 로컬인지에 대한 설정
+  const localPath = './question.txt';
+  const testPath = '/dev/stdin';
+  const isTest = process.platform === 'linux';
+  const filePath = isTest ? testPath : localPath;
+
+  const fileRead = require('fs').readFileSync(filePath);
+  // 한개
+  if (type === TYPES.SINGLE) return fileRead.toString().trim();
+  // 한줄
+  if (type === TYPES.ROW) return fileRead.toString().split(' ');
+  // Column
+  if (type === TYPES.COL) return fileRead.toString().trim().split('\n');
+
+  throw new Error('파일 읽는 방식 다시 확인해라');
+};
+
+//! 풀이
+const answer = () => {
+  //! 파일 읽기
+  const input = fileRead(TYPES.SINGLE);
+
+  const [N, M] = input.split(' ').map(Number);
+  const set = new Set();
+
+  const visited = Array(N + 1).fill(false);
+
+  const dfs = (depth, arr) => {
+    if (depth === M) {
+      set.add(arr.sort().join(' '));
+      return;
+    }
+
+    for (let i = 1; i <= N; i++) {
+      if (visited[i]) continue;
+      visited[i] = true;
+      dfs(depth + 1, [...arr, i]);
+      visited[i] = false;
+    }
+  };
+
+  dfs(0, []);
+
+  const result = [];
+
+  for (let value of set) {
+    result.push(value);
+  }
+
+  return result.join('\n');
+};
+
+//! 정답 출력
+console.log(answer());
