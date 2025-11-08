5f3cd15ff254f3cb29d3e112ac568c75a0b178ac
KimMinchan <83770790+KimMinchan95@users.noreply.github.com>
2025-11-08 10:10:38 +0900

    [Silver III] Title: 01타일, Time: 208 ms, Memory: 43940 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.js
@@ -0,0 +1,44 @@
+const { getActiveResourcesInfo } = require('process');
+
+const TYPES = {
+  SINGLE: 'SINGLE',
+  ROW: 'ROW',
+  COL: 'COL',
+};
+
+// ** 파일 읽는 방식들 ** //
+const fileRead = (type) => {
+  // 로컬인지에 대한 설정
+  const localPath = './question.txt';
+  const testPath = '/dev/stdin';
+  const isTest = process.platform === 'linux';
+  const filePath = isTest ? testPath : localPath;
+
+  const fileRead = require('fs').readFileSync(filePath);
+  // 한개
+  if (type === TYPES.SINGLE) return fileRead.toString().trim();
+  // 한줄
+  if (type === TYPES.ROW) return fileRead.toString().split(' ');
+  // Column
+  if (type === TYPES.COL) return fileRead.toString().trim().split('\n');
+
+  throw new Error('파일 읽는 방식 다시 확인해라');
+};
+
+//! 풀이
+const answer = () => {
+  //! 파일 읽기
+  const input = fileRead(TYPES.SINGLE);
+  const N = Number(input);
+
+  const memo = [0, 1, 2];
+
+  for (let i = 3; i <= N; i++) {
+    memo[i] = (memo[i - 2] + memo[i - 1]) % 15746;
+  }
+
+  return memo[N];
+};
+
+//! 정답 출력
+console.log(answer());
