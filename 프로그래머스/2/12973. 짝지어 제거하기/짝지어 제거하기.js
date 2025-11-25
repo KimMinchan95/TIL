03d8e2b1b26d44e35dd130a5f125b91bbe4be69b
KimMinchan <83770790+KimMinchan95@users.noreply.github.com>
2025-11-25 18:05:50 +0900

    [level 2] Title: 짝지어 제거하기, Time: 74.20 ms, Memory: 43.8 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,12 +1,14 @@
-function solution(s){
-    return Number(![...s].reduce((stack, cur) => {
-        
-        if (stack[stack.length - 1] === cur) {
+function solution(s)
+{
+    const stack = [];
+    
+    for (let i = 0; i < s.length; i++) {
+        if (stack.at(-1) === s[i]) {
             stack.pop();
-        } else {
-            stack.push(cur);
+            continue;
         }
-        
-        return stack;
-    }, []).length);
+        stack.push(s[i]);
+    }
+    
+    return Number(!Boolean(stack.length));
 }
