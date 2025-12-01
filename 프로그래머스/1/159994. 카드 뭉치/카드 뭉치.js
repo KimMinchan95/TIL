fe55b76fabd32f5a3d7961255871203f82e0924d
KimMinchan <83770790+KimMinchan95@users.noreply.github.com>
2025-12-01 18:51:52 +0900

    [level 1] Title: 카드 뭉치, Time: 0.30 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,14 +1,51 @@
+class Queue {
+    items = [];
+    front = 0;
+    rear = 0;
+    
+    constructor(array) {
+        this.items = array;
+        this.rear = array.length;
+    }
+    
+    push(item) {
+        this.items.push(item);
+        this.rear++;
+    }
+    
+    pop() {
+        return this.items[this.front++];
+    }
+    
+    first() {
+        return this.items[this.front];
+    }
+    
+    isEmpty() {
+        return this.front === this.rear;
+    }
+}
+
 function solution(cards1, cards2, goal) {
-    return goal.reduce((acc, cur) => {
-        if (cards1[0] === cur) {
-            cards1.shift();
-            return acc;
+    const queue1 = new Queue(cards1);
+    const queue2 = new Queue(cards2);
+    const goalQueue = new Queue(goal);
+    
+    while(!goalQueue.isEmpty()) {
+        if (queue1.first() === goalQueue.first()) {
+            queue1.pop();
+            goalQueue.pop();
+            continue;
         }
         
-        if (cards2[0] === cur) {
-            cards2.shift();
-            return acc;
+        if (queue2.first() === goalQueue.first()) {
+            queue2.pop();
+            goalQueue.pop();
+            continue;
         }
-        return 'No';
-    }, 'Yes');
+        
+        break;
+    }
+    
+    return goalQueue.isEmpty() ? 'Yes' : 'No';
 }
