177abe3998493c8d7a5bbabac216690ca857c973
KimMinchan <83770790+KimMinchan95@users.noreply.github.com>
2025-12-11 17:47:33 +0900

    [level 2] Title: 예상 대진표, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -0,0 +1,12 @@
+function solution(n,a,b)
+{
+    let result = 0;
+    
+    while (a !== b) {
+        a = Math.ceil(a / 2);
+        b = Math.ceil(b / 2);
+        result++;
+    }
+    
+    return result;
+}
