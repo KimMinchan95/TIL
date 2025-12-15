1e9aedb0bc79ed436fbca2b695176c34135480ab
KimMinchan <83770790+KimMinchan95@users.noreply.github.com>
2025-12-15 21:11:02 +0900

    [Gold V] Title: 평범한 배낭, Time: 208 ms, Memory: 11444 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.js
@@ -0,0 +1,48 @@
+const TYPES = {
+  SINGLE: 'SINGLE',
+  ROW: 'ROW',
+  COL: 'COL',
+};
+
+// ** 파일 읽는 방식들 ** //
+const fileRead = (type) => {
+  // 로컬인지에 대한 설정
+  const localPath = './question.txt';
+  const testPath = '/dev/stdin';
+  const isTest = process.platform === 'linux';
+  const filePath = isTest ? testPath : localPath;
+
+  const fileRead = require('fs').readFileSync(filePath);
+  // 한개
+  if (type === TYPES.SINGLE) return fileRead.toString().trim();
+  // 한줄
+  if (type === TYPES.ROW) return fileRead.toString().split(' ');
+  // Column
+  if (type === TYPES.COL) return fileRead.toString().trim().split('\n');
+
+  throw new Error('파일 읽는 방식 다시 확인해라');
+};
+
+//! 풀이
+const answer = () => {
+  //! 파일 읽기
+  const input = fileRead(TYPES.COL);
+
+  const [N, K] = input.shift().split(' ').map(Number);
+
+  const list = input.map((cur) => cur.split(' ').map(Number));
+
+  const dp = new Array(K + 1).fill(0);
+  dp[0] = 0;
+
+  for (const [W, V] of list) {
+    for (let i = K; i >= W; i--) {
+      dp[i] = Math.max(dp[i], dp[i - W] + V);
+    }
+  }
+
+  return Math.max(...dp);
+};
+
+//! 정답 출력
+console.log(answer());
