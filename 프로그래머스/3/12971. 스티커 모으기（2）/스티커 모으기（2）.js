22a0047b8076268b72d99efe561965e259d83775
KimMinchan <83770790+KimMinchan95@users.noreply.github.com>
2026-01-05 18:51:25 +0900

    [level 3] Title: 스티커 모으기(2), Time: 5.94 ms, Memory: 39.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.js
@@ -0,0 +1,21 @@
+function solution(sticker) {
+    const length = sticker.length;
+    const dp1 = Array(length).fill(0);
+    const dp2 = Array(length).fill(0);
+        
+    dp1[0] = sticker[0];
+    dp1[1] = sticker[0];
+    dp2[1] = sticker[1];
+
+    for(let i = 2; i < length; i++){
+        if(i !== length - 1){
+            dp1[i] = Math.max(dp1[i - 1], dp1[i - 2] + sticker[i]);
+        } else{
+            dp1[i] = dp1[i - 1];
+        }
+        
+        dp2[i] = Math.max(dp2[i - 1], dp2[i - 2] + sticker[i]);
+    }
+
+    return Math.max(dp1[length - 1], dp2[length - 1]);
+}
