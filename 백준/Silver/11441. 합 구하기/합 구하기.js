771fd8ae1860268285cb94ec401cb28918fedba3
KimMinchan <83770790+KimMinchan95@users.noreply.github.com>
2026-01-15 00:03:40 +0900

    [Silver III] Title: 합 구하기, Time: 316 ms, Memory: 45588 KB -BaekjoonHub

---
+++ 백준/Silver/11441. 합 구하기/합 구하기.js
@@ -0,0 +1,50 @@
+const TYPES = {
+  SINGLE: 'SINGLE',
+  ROW: 'ROW',
+  COL: 'COL',
+};
+
+// ** 파일 읽는 방식들 ** //
+const fileRead = (type) => {
+  // 로컬인지에 대한 설정
+  const localPath = './question.txt';
+  const testPath = '/dev/stdin';
+  const isTest = process.platform === 'linux';
+  const filePath = isTest ? testPath : localPath;
+
+  const fileRead = require('fs').readFileSync(filePath);
+  // 한개
+  if (type === TYPES.SINGLE) return fileRead.toString().trim();
+  // 한줄
+  if (type === TYPES.ROW) return fileRead.toString().split(' ');
+  // Column
+  if (type === TYPES.COL) return fileRead.toString().trim().split('\n');
+
+  throw new Error('파일 읽는 방식 다시 확인해라');
+};
+
+//! 풀이
+const answer = () => {
+  //! 파일 읽기
+  const input = fileRead(TYPES.COL);
+
+  const N = Number(input.shift());
+  const list = input.shift().split(' ').map(Number);
+  const M = Number(input.shift());
+
+  const prefix = new Array(N + 1).fill(0);
+  for (let i = 0; i < N; i++) {
+    prefix[i + 1] = prefix[i] + list[i];
+  }
+
+  return input
+    .reduce((acc, cur) => {
+      const [s, e] = cur.split(' ').map(Number);
+      acc.push(prefix[e] - prefix[s - 1]);
+      return acc;
+    }, [])
+    .join('\n');
+};
+
+//! 정답 출력
+console.log(answer());
