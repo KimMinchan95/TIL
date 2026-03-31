0c6445a469cdb4865c065b22dfb582e6e03080ec
KimMinchan <83770790+KimMinchan95@users.noreply.github.com>
2026-03-31 09:21:04 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 22.92 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -1,35 +1,31 @@
 function solution(n, wires) {
     const graph = Array.from({ length: n + 1 }, () => []);
-    wires.forEach(([s, e]) => {
-        graph[s].push(e);
-        graph[e].push(s);
+    wires.forEach(([a, b]) => {
+        graph[a].push(b);
+        graph[b].push(a);
     });
     
     const dfs = (node, parent) => {
         let count = 1;
-        for (const child of graph[node]) {
-            if (child !== parent) {
-                count += dfs(child, node);
-            }
-        }
+        graph[node].forEach(child => {
+            if (child === parent) return;
+            count += dfs(child, node);
+        });
         return count;
     }
     
     let minDiff = Infinity;
-    for (const [a, b] of wires) {
-        graph[a].splice(graph[a].indexOf(b), 1);
-        graph[b].splice(graph[b].indexOf(a), 1);
+    wires.forEach(([a, b]) => {
+        graph[a] = graph[a].filter(node => node !== b);
+        graph[b] = graph[b].filter(node => node !== a);
         
         const countA = dfs(a, b);
         const countB = n - countA;
-        
         minDiff = Math.min(minDiff, Math.abs(countA - countB));
         
         graph[a].push(b);
         graph[b].push(a);
-    }
+    });
     
     return minDiff;
-    
-    
 }
