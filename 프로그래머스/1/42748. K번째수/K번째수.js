c6b73e37cb4130ff1cd57853416d31a1ec8ef75c
KimMinchan <83770790+KimMinchan95@users.noreply.github.com>
2026-05-12 11:21:18 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,7 +1,1 @@
-function solution(array, commands) {
-    return commands.reduce((acc, [i,j,k]) => {
-        const temp = array.slice(i - 1, j);
-        temp.sort((a, b) => a - b);
-        return [...acc, temp[k - 1]];
-    }, []);
-}
+const solution = (array, commands) => commands.reduce((acc, [i,j,k]) => [...acc, array.slice(i - 1, j).sort((a, b) => a - b)[k - 1]], []);
