53f4d0cf7dd0c8d4130a2b6120f376dac40fa7b2
KimMinchan <83770790+KimMinchan95@users.noreply.github.com>
2026-05-18 17:34:49 +0900

    [level 2] Title: 카펫, Time: 0.16 ms, Memory: 32.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,7 +1,7 @@
 function solution(brown, yellow) {
     const total = brown + yellow;
     
-    for (let x = 0; x <= Math.sqrt(total); x++) {
+    for (let x = 3; x <= Math.sqrt(total); x++) {
         if (total % x === 0) {
             const y = total / x;
             if (yellow === (x - 2) * (y - 2)) {
